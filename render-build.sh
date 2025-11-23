50898577eeaaa191c481f66354b3d8c30941629f
SoraDownloader <sora@downloader.com>
2025-11-23 17:34:10 -0600

    Arreglado build - Eliminado script problemático

--- render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Instalar dependencias de Node
-npm install
-
-# Instalar Chromium y dependencias
-apt-get update
-apt-get install -y chromium chromium-sandbox
-
-echo "Build completado con Chromium instalado"
